10dd0721db455f479a5c9c339e266a7fd9ac6e90
ElieBulakali <137182964+ElieBulakali@users.noreply.github.com>
2025-10-30 17:45:30 +0200

    Delete Kazi-spot directory

--- Kazi-spot/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
-      Hello, Tailwind + React!
-    </div>
-  );
-}
-
-export default App;
